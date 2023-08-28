6e328e49635df9f7f11fe7f40cfa9e519517113e
Masoumeh Rezai <you@example.com>
2023-08-28 23:07:41 +0430

    add project

--- src/App.js
+++ src/App.js
@@ -1,46 +1,12 @@
-import { Fragment, useState } from "react";
-import Home from "./Componets/Home/Home";
-import About from "./Componets/About/About";
-import Line from "./Componets/About/Line";
-import Product from "./Componets/Product/Product";
-import ErroModal from "./Componets/UI/ErrorModal";
-import Information from "./Componets/Information/Information";
-import Form from "./Componets/Form/Form";
-import FormModule from './Componets/Form/FormModule';
 
 
 
 function App() {
-    const [add, setAdd] = useState(false);
-    const [form, setform] = useState(false);
-    const showCart = () => {
-        setAdd(true);
 
-    }
-    const hideCart = () => {
-        setAdd(false)
-    }
-    const formTrue = () => {
-        setform(true)
-    }
-    const formFalse = () => {
-        setform(false)
-    }
 
 
     return (
-        < Fragment >
-            <Home />
-            <About />
-            <Line />
-            <Information />
-            <Product onshow={showCart} />
-            {add && <ErroModal onhide={hideCart} />}
-            <Form onform={formTrue} />
-            {form && <FormModule ofform={formFalse} />}
-
-
-        </Fragment >
+        <h1>hello</h1>
     )
 }
 
--- src/Componets/Form/Button.js
+++
@@ -1,9 +0,0 @@
-import classes from './Form.module.css'
-const Button = (props) => {
-    return (
-        <div>
-            <button className={classes.btn} onClick={props.onClick}>{props.children}</button>
-        </div>
-    )
-}
-export default Button;
