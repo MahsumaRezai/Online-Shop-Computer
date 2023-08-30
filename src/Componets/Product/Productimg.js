e03c30f327268240b224d5cd83a8b09a238729a7
Masoumeh Rezai <you@example.com>
2023-08-30 06:38:47 +0430

    add img

--- src/Componets/Product/Productimg.js
+++ src/Componets/Product/Productimg.js
@@ -2,6 +2,7 @@ import { Fragment } from "react";
 import img1 from '../img/1.png';
 import img2 from '../img/2.png';
 import img3 from '../img/3.png';
+import img4 from '../img/star.png'
 import classes from './Product.module.css';
 
 
@@ -9,16 +10,25 @@ import classes from './Product.module.css';
 const Productimg = (props) => {
     return (
         <Fragment>
-            <div>
-                <div>
+            <div className={classes.SectionImg}>
+                <div className={classes.cart}>
                     <img src={img1} alt="" />
+                    <p>100$</p>
+                    <p>Mobile Phone</p>
+                    <img src={img4} alt="" />
                 </div>
-                <div>
+                <div className={classes.cart}>
                     <img src={img2} alt="" />
+                    <p>100$</p>
+                    <p>Mobile Phone</p>
+                    <img src={img4} alt="" />
                 </div>
 
-                <div>
+                <div className={classes.cart}>
                     <img src={img3} alt="" />
+                    <p>100$</p>
+                    <p>Mobile Phone</p>
+                    <img src={img4} alt="" />
                 </div>
 
             </div>
