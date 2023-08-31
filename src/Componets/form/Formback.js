bc8a62a71c3d6cc76b9aeeb45eec17c32a13e902
Masoumeh Rezai <you@example.com>
2023-08-31 07:40:33 +0430

    add form style

--- src/Componets/form/Formback.js
+++ src/Componets/form/Formback.js
@@ -1,16 +1,23 @@
 import { Fragment } from "react";
-import classes from './Form.module.css'
+import classes from './Form.module.css';
 
 const Formback = (props) => {
     return (
         <Fragment>
             <div className={classes.form}>
-                <div>
+                <h1 className={classes.title}>Request a Call back</h1>
+                <form className={classes.context}>
+                    <input placeholder='Name' />
+                    <input placeholder='Phone Number' />
+                    <input placeholder='Email' />
+                    <input placeholder='Massage' />
+                    <button className={classes.btn}> Send</button>
 
-                </div>
-                <div>
 
-                </div>
+
+                </form>
+
+
 
             </div>
 
