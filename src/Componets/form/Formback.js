074c46b36b074d585e8bc3322ef47f87cc6846f1
Masoumeh Rezai <you@example.com>
2023-09-01 22:23:20 +0430

    add form vaildation

--- src/Componets/form/Formback.js
+++ src/Componets/form/Formback.js
@@ -6,15 +6,27 @@ const Formback = (props) => {
     const [number, setNumber] = useState('');
     const [email, setEmail] = useState('');
     const [massage, setMassage] = useState('');
-     /*const [nameCheck,setNameCaheck]=useState(false);
-    const [numCheck,setNumCaheck]=useState(false);
-    const [emailCheck,setEmaiCaheck]=useState(false);
-    const [massCheck,setMassCaheck]=useState(false);
-*/
+    const [nameCheck, setNameCaheck] = useState(false);
+    const [numCheck, setNumCaheck] = useState(false);
+    const [emailCheck, setEmaiCaheck] = useState(false);
+    const [massCheck, setMassCaheck] = useState(false);
+
 
     const formHandler = (event) => {
         event.preventDefault();
-        console.log(name, email, number, massage)
+        if (name.trim().length === 0) {
+            setNameCaheck(true)
+
+        }
+        if (number.trim().length === 0) {
+            setNumCaheck(true)
+        }
+        if (email.trim().length === 0) {
+            setEmaiCaheck(true)
+        }
+        if (massage.trim().length === 0) {
+            setMassCaheck(true)
+        }
 
     }
     const Name = (event) => {
@@ -41,9 +53,13 @@ const Formback = (props) => {
                 <h1 className={classes.title}>Request a Call back</h1>
                 <form className={classes.context} onSubmit={formHandler}>
                     <input placeholder='Name' onChange={Name} />
+                    {nameCheck && <p className={classes.para}>Name is requried</p>}
                     <input placeholder='Phone Number ' onChange={Nunber} />
+                    {numCheck && <p className={classes.para}>Phone Number is requried</p>}
                     <input placeholder='Email' onChange={Email} />
+                    {emailCheck && <p className={classes.para}>Email is requried</p>}
                     <input placeholder='Massage' onChange={Massage} />
+                    {massCheck && <p className={classes.para}>Massage is requried</p>}
                     <button className={classes.btn}> Send</button>
 
 
