38da33d38ff04294f346e396a9fad6c4d5856cb0
Masoumeh Rezai <you@example.com>
2023-09-01 05:59:47 +0430

    add last commit for footer

--- src/Componets/Footer/Footer.js
+++ src/Componets/Footer/Footer.js
@@ -1,8 +1,16 @@
 import classes from './Footer.module.css';
+import img from '../img/profile.jpg'
 
 const Footer = (props) => {
     return (
         <div className={classes.footer}>
+            <h1 className={classes.title}> Call Now</h1>
+            <div className={classes.warp}>
+                <img src={img} alt="" className={classes.img} />
+                <p>mahsuma.rezai8@gmail.com</p>
+                <p>&</p>
+                <p>@MahsumaRezai</p>
+            </div>
 
 
         </div>
