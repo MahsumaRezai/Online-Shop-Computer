d62ae846bd8a8d83cb78d73e3c1477331dca37d9
Masoumeh Rezai <you@example.com>
2023-09-01 08:13:21 +0430

    last commit for today

--- src/Componets/form/Formback.js
+++ src/Componets/form/Formback.js
@@ -1,16 +1,49 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import classes from './Form.module.css';
 
 const Formback = (props) => {
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    const [email, setEmail] = useState('');
+    const [massage, setMassage] = useState('');
+     /*const [nameCheck,setNameCaheck]=useState(false);
+    const [numCheck,setNumCaheck]=useState(false);
+    const [emailCheck,setEmaiCaheck]=useState(false);
+    const [massCheck,setMassCaheck]=useState(false);
+*/
+
+    const formHandler = (event) => {
+        event.preventDefault();
+        console.log(name, email, number, massage)
+
+    }
+    const Name = (event) => {
+        setName(event.target.value);
+
+    }
+    const Nunber = (event) => {
+        setNumber(event.target.value);
+
+    }
+    const Email = (event) => {
+        setEmail(event.target.value);
+
+    }
+    const Massage = (event) => {
+        setMassage(event.target.value);
+
+    }
+
+
     return (
         <Fragment>
             <div className={classes.form}>
                 <h1 className={classes.title}>Request a Call back</h1>
-                <form className={classes.context}>
-                    <input placeholder='Name' />
-                    <input placeholder='Phone Number' />
-                    <input placeholder='Email' />
-                    <input placeholder='Massage' />
+                <form className={classes.context} onSubmit={formHandler}>
+                    <input placeholder='Name' onChange={Name} />
+                    <input placeholder='Phone Number ' onChange={Nunber} />
+                    <input placeholder='Email' onChange={Email} />
+                    <input placeholder='Massage' onChange={Massage} />
                     <button className={classes.btn}> Send</button>
 
 
